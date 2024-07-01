dabd3b06f6be8ddcf13b73a5b4e459c501fd3a3d
Qkenza <kmerouar@gmail.com>
2024-06-30 19:35:27 -0700

    configmanagement

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
-# Install flask version 2.1.0 using pip3.
-
-package { 'flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-# Ensure Werkzeug version 2.1.1
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
+  provider => 'pip3'
 }
